e412cd52e38cd53aa76aad66b4043177b016fa3f
Winnux Reboxyz <reboxyz@gmail.com>
2024-01-06 21:57:08 +0900

    config() and env()

---
+++ config/livepost.php
@@ -0,0 +1,15 @@
+<?php 
+
+return [
+    'heya' => [
+        'heya_inner' => 'This is heya inner value',
+        'hello' => 'Hello, Winnux',
+    ],
+
+    'config_test' => [
+        'test1' => 'test value for test 1',
+        'test2' => 'test value for test 2',
+    ],
+];
+
+?>
